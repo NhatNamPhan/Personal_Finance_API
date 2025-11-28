84f47c276701b0efce69ae539ea5b11b250515f8
Nhat Nam <22115053122323@sv.ute.udn.vn>
2025-11-28 10:37:54 +0700

    router accounts

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from app.routers import users, accounts, transactions, categories, budgets
 
 app = FastAPI(title="Personal Finance API")
 
-# app.include_router(accounts.router)
+app.include_router(accounts.router)
 # app.include_router(budgets.router)
 # app.include_router(categories.router)
 # app.include_router(transactions.router)
---
+++ app/routers/accounts.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, HTTPException
+from app.database import get_db
+from app.utils import check_exists
+from app.models import Account
+import psycopg2
+
+router = APIRouter(prefix="/accounts", tags=['Accounts'])
+
+@router.post("")
+async def add_account(account: Account):
+    try:
+        with get_db() as cur:
+            cur.execute(
+                "INSERT INTO accounts (user_id, name, balance, type) VALUES(%s, %s, %s, %s) RETURNING account_id",
+                (account.user_id, account.name, account.balance, account.type)
+            )
+            new_id = cur.fetchone()[0]
+            return {"message": "Account added successful", "account_id": new_id}
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {e.pgerror}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")  
+   
+@router.get("")
+async def get_account_user_id(user_id: int):
+    try:
+        with get_db() as cur:
+            check_exists("users", "user_id", user_id)
+            cur.execute(
+                "SELECT * FROM accounts WHERE user_id = %s", (user_id,)
+            )
+            rows = cur.fetchall()
+            accounts = [{
+                "account_id": row[0],
+                "name": row[2],
+                "balance": row[3],
+                "type": row[4]
+            } for row in rows]
+            return [{
+                "user_id": user_id,
+                "accounts": accounts
+            }]
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {e.pgerror}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")    
