6c33903005c673f0e0c558eab8b19435e7ce0752
Nhat Nam <22115053122323@sv.ute.udn.vn>
2025-11-30 22:25:40 +0700

    router-budgets

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.routers import users, accounts, transactions, categories, budgets
 app = FastAPI(title="Personal Finance API")
 
 app.include_router(accounts.router)
-# app.include_router(budgets.router)
+app.include_router(budgets.router)
 app.include_router(categories.router)
 app.include_router(transactions.router)
 app.include_router(users.router)
---
+++ app/routers/budgets.py
@@ -0,0 +1,128 @@
+from fastapi import APIRouter, HTTPException
+from app.database import get_db
+from app.utils import check_exists
+from app.models import Budget, BudgetUpdate
+import psycopg2
+from datetime import date
+
+router = APIRouter(prefix="/budgets", tags=['Budgets'])
+
+@router.post("")
+async def add_budget(budget: Budget):
+    try:
+        with get_db() as cur:
+            cur.execute(
+                "INSERT INTO budgets(user_id, category_id, amount, month) VALUES(%s, %s, %s, %s) RETURNING budget_id",
+                (budget.user_id, budget.category_id, budget.amount, budget.month)
+            )
+            new_id = cur.fetchone()[0]
+            return {"message": "Budget added successfully", "budget_id": new_id}
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {e.pgerror}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
+    
+@router.get("")
+async def get_budget(user_id: int, month: date | None = None):
+    try:
+        with get_db() as cur:
+            check_exists("users", "user_id", user_id)
+            if month is None:
+                cur.execute(
+                    "SELECT * FROM budgets WHERE user_id = %s", (user_id,)
+                )
+                rows = cur.fetchall()
+                budgets = [{
+                    "budget_id": row[0],
+                    "category_id": row[2],
+                    "amount": row[3],
+                    "month": row[4]
+                } for row in rows]
+                return {
+                    "user_id": user_id,
+                    "budgets": budgets
+                }
+            else:
+                cur.execute('''
+                    SELECT * FROM budgets
+                    WHERE user_id = %s 
+                        AND month = %s
+                ''', (user_id, month))
+                rows = cur.fetchall()
+                budgets = [{
+                    "budget_id": row[0],
+                    "category_id": row[2],
+                    "amount": row[3],  
+                } for row in rows]
+                return {
+                    "user_id": user_id,
+                    "month": month,
+                    "budgets": budgets
+                }
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail=f"Database error : {e.pgerror}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
+
+@router.get("/{budget_id}")
+async def get_budget_by_id(budget_id: int):
+    try:
+        with get_db() as cur:
+            check_exists("budgets", "budget_id", budget_id)
+            cur.execute(
+                "SELECT * FROM budgets WHERE budget_id = %s", (budget_id,)
+            )
+            row = cur.fetchone()
+            return {
+                "budget_id": row[0],
+                "user_id": row[1],
+                "category_id": row[2],
+                "amount": row[3],
+                "month": row[4],
+                "created_at": row[5]
+            }
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {e.pgerror}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
+
+@router.put("/{budget_id}")           
+async def update_budget(budget_id: int, update: BudgetUpdate):
+    try:
+        with get_db() as cur:
+            check_exists("budgets", "budget_id", budget_id)
+            cur.execute('''
+                UPDATE budgets
+                SET amount = %s,
+                    month = %s
+                WHERE budget_id = %s
+                RETURNING budget_id, user_id, category_id, amount, month, created_at
+            ''', (update.amount, update.month, budget_id))
+            row = cur.fetchone()
+            return {
+                "budget_id": row[0],
+                "user_id": row[1],
+                "category_id": row[2],
+                "amount": row[3],
+                "month": row[4],
+                "created_at": row[5]
+            }
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {e.pgerror}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
+
+@router.delete("/{budget_id}")
+async def delete_budget(budget_id: int):
+    try:
+        with get_db() as cur:
+            check_exists("budgets", "budget_id", budget_id)
+            cur.execute(
+                "DELETE FROM budgets WHERE budget_id = %s", (budget_id,)
+            )        
+            return {"message": f"Budget with id {budget_id} has been deleted successfully"}    
+    except psycopg2.Error as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {e.pgerror}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
+
